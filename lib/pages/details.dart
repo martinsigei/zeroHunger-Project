8f871c2b6e8e3d564213676bc8749d8066f60e03
Martin Sigei <sigeimartin03@gmail.com>
2024-09-03 01:20:58 +0300

    Update  details.dart

--- lib/pages/details.dart
+++ lib/pages/details.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:zerohunger_logistics_app/widget/widget_support.dart';
 
 class Details extends StatefulWidget{
   const Details ({super.key});
@@ -7,23 +8,120 @@ class Details extends StatefulWidget{
   State<Details> createState()=> _DetailsState();
 }
   class  _DetailsState extends State<Details>{
+    int a=1;
     
     @override
     Widget build(BuildContext context){
       return Scaffold(
          appBar: AppBar(
-        title: Text('Details Page'),
+        title: Text('Fruits & Vegetables Page'),
       ),
-        body: Container(
+        body: 
+        SingleChildScrollView(
+        child:Container(
           margin: EdgeInsets.only(top: 50.0,left: 20.0,right: 20.0),
           child: Column(children: [
            ClipOval(
             child: Image.asset("images/fruits.jpeg",
-                          width: MediaQuery.of(context).size.width,
-                          height: MediaQuery.of(context).size.height/2,fit: BoxFit.fill,),     
+                          width: MediaQuery.of(context).size.width/1.2,
+                          height: MediaQuery.of(context).size.height/3,fit: BoxFit.fill,),     
                              ),
-                                
-        ],),),
+                    SizedBox(height: 15.0,),
+                    Row(
+                    
+                      children: [
+                       Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                         children: [
+                           Text("Fruits & Vegetables",style: AppWidget.SemiBoldTextFeildStyle(),),
+                            Text("Fresh and Healthy",style: AppWidget.HeadTextFeildStyle(),),
+                         ],
+                       ),
+                       Spacer(),
+                      GestureDetector(
+                        onTap: () {
+                          if(a>1){
+                          --a;
+                          }
+                          setState(() {
+                            
+                          });
+                        },
+                        child: Container(
+                        
+                          decoration: BoxDecoration(color: Colors.black,borderRadius: BorderRadius.circular(8)),
+                          child: Icon(Icons.remove,color: Colors.white,),
+                        ),
+                      ),
+                      SizedBox(width: 20.0,),
+                      Text(a.toString(),style: AppWidget.SemiBoldTextFeildStyle(),),
+                       SizedBox(width: 20.0,),
+                      GestureDetector(
+                        onTap: () {
+                          ++a;
+                          setState(() {
+                            
+                          });
+                        },
+                        child: Container(
+                        
+                          decoration: BoxDecoration(color: Colors.black,borderRadius: BorderRadius.circular(8)),
+                          child: Icon(Icons.add,color: Colors.white,),
+                        ),
+                      )
+                    ],),
+                    SizedBox(height: 20.0,),
+                    Text("At ZeroHunger Logistics, our focus on fruits and vegetables is driven by a commitment to providing fresh, locally sourced produce directly from farmers to consumers and traders. By bridging the gap between farm and table, we aim to ensure that quality, nutritious fruits and vegetables are accessible to everyone, promoting healthier diets and supporting local agriculture. Our streamlined digital platform enables individuals and traders to place bulk orders efficiently, allowing farmers to respond promptly and drivers to deliver goods swiftly. This approach not only reduces food waste but also enhances food security by minimizing the time produce spends in transit, preserving its freshness and nutritional value.",style: AppWidget.LightTextFeildStyle(),
+                    ),
+                    SizedBox(height: 30.0,),
+                    Row(children: [
+                      Text("Delivery Time",style: AppWidget.SemiBoldTextFeildStyle(),),
+                       SizedBox(width: 25.0,),
+                      Icon(Icons.alarm,color: Colors.black54,),
+                      SizedBox(width: 5.0,),
+                      Text("30 min",style: AppWidget.SemiBoldTextFeildStyle(),)
+                      ],
+                    ),
+                    //Spacer(),
+                    Padding(
+                      padding: const EdgeInsets.only(bottom: 40.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                          Text("Total Price",style: AppWidget.SemiBoldTextFeildStyle(),),
+                           Text("\$25/Kilogram",style: AppWidget.HeadTextFeildStyle(),)
+                        ],),
+                        Container(
+                           width: MediaQuery.of(context).size.width/2,
+                          padding: EdgeInsets.all(8),
+                          decoration: BoxDecoration(color: Colors.black,borderRadius: BorderRadius.circular(10)),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            children: [
+                            Text("Add to Cart",style: TextStyle(color: Colors.white,fontSize: 16.0,fontFamily: 'Poppins'),),
+                            SizedBox(width: 30.0,),
+                            Container(
+
+                             
+                              padding: EdgeInsets.all(3),
+                              decoration: BoxDecoration(
+                                color: Colors.black45,borderRadius: BorderRadius.circular(8),
+                              ),
+                              child: Icon(Icons.shopping_cart_outlined,color: Colors.white,),
+                            ),
+                             SizedBox(width: 10.0,),
+                          ],),
+
+                        )
+                      ],),
+                    )
+        ],
+        ),
+        ),
+      ),
       );
     }
   }
