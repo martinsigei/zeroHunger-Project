74a057eb75c8aa464bd7fa13aed2982ae477fa6b
Martin Sigei <sigeimartin03@gmail.com>
2024-09-18 18:26:37 +0300

    Update profile.dart

--- lib/pages/profile.dart
+++ lib/pages/profile.dart
@@ -1,15 +1,248 @@
+import 'dart:io';  
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:random_string/random_string.dart';
+import 'package:zerohunger_logistics_app/service/shared_pref.dart';
+import 'package:zerohunger_logistics_app/service/auth.dart';
 
-class Profile extends StatefulWidget{
-  const Profile ({super.key});
+class Profile extends StatefulWidget {
+  const Profile({super.key});
 
   @override
-  State<Profile> createState()=> _ProfileState();
+  State<Profile> createState() => _ProfileState();
 }
-  class  _ProfileState extends State<Profile>{
-    
-    @override
-    Widget build(BuildContext context){
-      return Container();
+
+class _ProfileState extends State<Profile> {
+  final ImagePicker _picker = ImagePicker();
+  dynamic selectedImage;
+  String? profile, name, email;
+  bool loading = true;  // Loading state
+
+  Future getImage() async {
+    if (kIsWeb) {
+      // Web image picker logic (commented out for now)
+    } else {
+      final image = await _picker.pickImage(source: ImageSource.gallery);
+      if (image != null) {
+        setState(() {
+          selectedImage = image.path;
+        });
+      }
     }
   }
+
+  Future uploadItem() async {
+    if (selectedImage != null) {
+      String addId = randomAlphaNumeric(10);
+      Reference firebaseStorageRef =
+          FirebaseStorage.instance.ref().child("blogImages").child(addId);
+
+      UploadTask task;
+      if (kIsWeb) {
+        // Web upload
+        task = firebaseStorageRef.putData(selectedImage);
+      } else {
+        // Mobile upload
+        task = firebaseStorageRef.putFile(File(selectedImage));
+      }
+
+      var downloadUrl = await (await task).ref.getDownloadURL();
+      await SharedPreferenceHelper().SaveUserProfile(downloadUrl);
+      setState(() {});
+    }
+  }
+
+  Future getSharedPrefData() async {
+    profile = await SharedPreferenceHelper().getUserProfile();
+    name = await SharedPreferenceHelper().getUserName();
+    email = await SharedPreferenceHelper().getUserEmail();
+    setState(() {
+      loading = false; // Data loading is complete
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getSharedPrefData();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: loading
+          ? Center(child: CircularProgressIndicator()) // Show spinner while loading
+          : Container(
+              child: Column(
+                children: [
+                  Stack(
+                    children: [
+                      Container(
+                        padding: EdgeInsets.only(top: 45.0, left: 20.0, right: 20.0),
+                        height: MediaQuery.of(context).size.height / 4.3,
+                        width: MediaQuery.of(context).size.width,
+                        decoration: BoxDecoration(
+                          color: Colors.black,
+                          borderRadius: BorderRadius.vertical(
+                            bottom: Radius.elliptical(
+                                MediaQuery.of(context).size.width, 105.0),
+                          ),
+                        ),
+                      ),
+                      Center(
+                        child: Container(
+                          margin: EdgeInsets.only(
+                              top: MediaQuery.of(context).size.height / 6.5),
+                          child: Material(
+                            elevation: 10.0,
+                            borderRadius: BorderRadius.circular(60),
+                            child: ClipRRect(
+                              borderRadius: BorderRadius.circular(60.0),
+                              child: selectedImage == null
+                                  ? GestureDetector(
+                                      onTap: () {
+                                        getImage();
+                                      },
+                                      child: profile == null
+                                          ? Image.asset(
+                                              "images/boy.jpg",
+                                              height: 120,
+                                              width: 120,
+                                              fit: BoxFit.cover,
+                                            )
+                                          : Image.network(
+                                              profile!,
+                                              height: 120,
+                                              width: 120,
+                                              fit: BoxFit.cover,
+                                            ),
+                                    )
+                                  : Image.file(File(selectedImage)),
+                            ),
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(top: 70.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              name ?? "Unknown User",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 25.0,
+                                fontWeight: FontWeight.bold,
+                                fontFamily: 'Poppins',
+                              ),
+                            ),
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
+                  SizedBox(height: 20.0),
+                  buildInfoTile(Icons.person, "Name", name ?? "N/A"),
+                  SizedBox(height: 30.0),
+                  buildInfoTile(Icons.email, "Email", email ?? "N/A"),
+                  SizedBox(height: 30.0),
+                  buildInfoTile(Icons.description, "Terms and Conditions", ""),
+                  SizedBox(height: 30.0),
+                  buildActionTile(Icons.delete, "Delete Account", () {
+                    AuthMethods().deleteuser();
+                  }),
+                  SizedBox(height: 30.0),
+                  buildActionTile(Icons.logout, "LogOut", () async {
+                    print("Logout clicked");  // Check if the tap is detected
+                    bool success = await AuthMethods().SignOut();
+                    if (success) {
+                      // Navigate back or show a confirmation
+                      print("User logged out successfully");
+                    } else {
+                      // Handle failure
+                      print("Logout failed");
+                    }
+                  }),
+                ],
+              ),
+            ),
+    );
+  }
+
+  Widget buildInfoTile(IconData icon, String title, String info) {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 20.0),
+      child: Material(
+        borderRadius: BorderRadius.circular(10),
+        elevation: 2.0,
+        child: Container(
+          padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 10.0),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(10),
+          ),
+          child: Row(
+            children: [
+              Icon(icon, color: Colors.black),
+              SizedBox(width: 20.0),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    title,
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 16.0,
+                        fontWeight: FontWeight.w600),
+                  ),
+                  Text(
+                    info,
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 16.0,
+                        fontWeight: FontWeight.w600),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget buildActionTile(IconData icon, String title, VoidCallback onTap) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        margin: EdgeInsets.symmetric(horizontal: 20.0),
+        child: Material(
+          borderRadius: BorderRadius.circular(10),
+          elevation: 2.0,
+          child: Container(
+            padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 10.0),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: Row(
+              children: [
+                Icon(icon, color: Colors.black),
+                SizedBox(width: 20.0),
+                Text(
+                  title,
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.w600),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
