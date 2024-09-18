e08ea12cc90362049d62dabea60b06c928bb5777
Martin Sigei <sigeimartin03@gmail.com>
2024-09-18 18:24:38 +0300

    Update login.dart

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,4 +1,7 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:zerohunger_logistics_app/pages/bottomnav.dart';
+import 'package:zerohunger_logistics_app/pages/forgotpassword.dart';
 import 'package:zerohunger_logistics_app/pages/signup.dart';
 import 'package:zerohunger_logistics_app/widget/widget_support.dart';
 
@@ -9,11 +12,44 @@ class LogIn extends StatefulWidget{
   State<LogIn> createState()=> _LogInState();
 }
   class  _LogInState extends State<LogIn>{
+  String email="",Password="";
+
+  final _formkey=GlobalKey<FormState>();
+
+  TextEditingController useremailcontroller=new TextEditingController();
+  TextEditingController userpasswordcontroller=new TextEditingController();
+
+  userLogin()async{
+    try{
+      await FirebaseAuth.instance.
+      signInWithEmailAndPassword(email: email, password: Password);
+       ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+       backgroundColor: Colors.redAccent,
+      content:Text( "Login Successfully",style:TextStyle(fontSize: 20.0),),),);
+      Navigator.pushReplacement(context, MaterialPageRoute(builder:(context)=>BottomNav() ));
+    }on FirebaseAuthException catch(e){
+      if(e.code=='user-not-found'){
+        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+          content: Text(
+            "No User Found for that Email",
+            style: TextStyle(fontSize: 18.0,color: Colors.black),
+            ),),);
+      }else if(e.code=='wrong-password'){
+         ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+          content: Text(
+            "Wrong Password Provided by User",
+            style: TextStyle(fontSize: 18.0,color: Colors.black),
+            ),),);
+      }
+    }
+  }
     
     @override
     Widget build(BuildContext context){
       return Scaffold(
-        body: Container(
+        body: SingleChildScrollView(  // Added Scrollable View
+      child: Container(
+        height: MediaQuery.of(context).size.height,
           child: Stack(
           children: [
           Container(
@@ -46,39 +82,72 @@ class LogIn extends StatefulWidget{
         width: MediaQuery.of(context).size.width,
         height: MediaQuery.of(context).size.height/2,
         decoration: BoxDecoration(color: Colors.grey,borderRadius: BorderRadius.circular(20)),
-        child: Column(children: [
-          SizedBox(height: 30.0,),
-          Text("Login",style: AppWidget.HeadTextFeildStyle(),),
-          SizedBox(
-            height: 30.0,
-          ),
-          TextField(
-            decoration: InputDecoration(hintText: 'Email',hintStyle: AppWidget.SemiBoldTextFeildStyle(),prefixIcon: Icon(Icons.email_outlined),
-          ),),
+        child: Form(
+          key: _formkey,
+          child: Column(children: [
+            SizedBox(height: 30.0,),
+            Text("Login",style: AppWidget.HeadTextFeildStyle(),),
             SizedBox(
-            height: 30.0,
-          ),
-          TextField(
-            obscureText: true,
-            decoration: InputDecoration(hintText: 'Password',hintStyle: AppWidget.SemiBoldTextFeildStyle(),prefixIcon: Icon(Icons.password_rounded),
-          ),
-          ),
-          SizedBox(height: 20.0,),
-          Container(
-            alignment: Alignment.topRight,
-            child: Text("Forgot Password?",style: AppWidget.SemiBoldTextFeildStyle(),)),
-            SizedBox(height: 60.0,),
-            Material(
-              elevation:5.0,
-              borderRadius: BorderRadius.circular(20),
-              child: Container(
-                padding: EdgeInsets.symmetric(vertical: 8.0),
-                width: 200,
-                 decoration: BoxDecoration(color: Color.fromARGB(255, 52, 155, 55),borderRadius: BorderRadius.circular(20)),
-                child: Center(child: Text("LOGIN",style: TextStyle(color: Colors.black,fontSize: 18.0,fontFamily: 'Poppins1',fontWeight: FontWeight.bold),)),
-              ),
+              height: 30.0,
             ),
-        ],
+            TextFormField(
+              controller: useremailcontroller,
+              validator: (value) {
+                if(value==null||value.isEmpty){
+                  return 'Please Enter Email';
+                }
+                return null;
+              },
+              decoration: InputDecoration(hintText: 'Email',hintStyle: AppWidget.SemiBoldTextFeildStyle(),prefixIcon: Icon(Icons.email_outlined),
+            ),),
+              SizedBox(
+              height: 30.0,
+            ),
+            TextFormField(
+              controller: userpasswordcontroller,
+              validator: (value) {
+                if(value==null||value.isEmpty){
+                  return 'Please Enter Password';
+                }
+                return null;
+              },
+              obscureText: true,
+              decoration: InputDecoration(hintText: 'Password',hintStyle: AppWidget.SemiBoldTextFeildStyle(),prefixIcon: Icon(Icons.password_rounded),
+            ),
+            ),
+            SizedBox(height: 20.0,),
+            GestureDetector(
+              onTap: () {
+                Navigator.push(context, MaterialPageRoute(builder: (context)=>ForgotPassword()));
+              },
+              child: Container(
+                alignment: Alignment.topRight,
+                child: Text("Forgot Password?",style: AppWidget.SemiBoldTextFeildStyle(),)),
+            ),
+              SizedBox(height: 60.0,),
+              GestureDetector(
+                onTap: () {
+                  if(_formkey.currentState!.validate()){
+                    setState(() {
+                      email=useremailcontroller.text;
+                      Password=userpasswordcontroller.text;
+                    });
+                    userLogin();
+                  }
+                },
+                child: Material(
+                  elevation:5.0,
+                  borderRadius: BorderRadius.circular(20),
+                  child: Container(
+                    padding: EdgeInsets.symmetric(vertical: 8.0),
+                    width: 200,
+                     decoration: BoxDecoration(color: Color.fromARGB(255, 52, 155, 55),borderRadius: BorderRadius.circular(20)),
+                    child: Center(child: Text("LOGIN",style: TextStyle(color: Colors.black,fontSize: 18.0,fontFamily: 'Poppins1',fontWeight: FontWeight.bold),)),
+                  ),
+                ),
+              ),
+          ],
+          ),
         ),
            ),
      ),
@@ -92,6 +161,6 @@ class LogIn extends StatefulWidget{
     ),
    
         ],),),
-      );
+        ));
     }
   }
